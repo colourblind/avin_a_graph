b219c60bc8afbdac47cc97c05ee10b198de702ca
colourblind <tom.milsom@gmail.com>
2012-03-02 00:05:53 +0000

    Separated nodes and edges. Still very buggy

--- graph_svg.js
+++ graph_svg.js
@@ -1,76 +1,92 @@
-function graph_svg(target)
+function graph_svg(target, nodes, edges)
 {
     var r = Raphael(target, 500, 500);
-    
-    var root = {x: 0, y: 0, children: []};
-    root.children.push({x: 60, y: 0, children: []});
-    root.children.push({x: 80, y: 0, children: []});
-    root.children.push({x: 12, y: 100, children: []});
-    root.children.push({x: 0, y: -20, children: []});
-    root.children[0].children.push({x: 0, y: 0, children: []});
-    root.children[0].children.push({x: 0, y: 0, children: []});
-    
-    this.render = function(e)
+ 
+    for (var i = 0; i < nodes.length; i ++)
     {
-        for (var i = 0; i < e.children.length; i ++)
+        nodes[i].x = Math.random() * 10 - 5;
+        nodes[i].y = Math.random() * 10 - 5;
+        nodes[i].force = { x: 0, y: 0 };
+        nodes[i].size = 0;
+        for (var j = 0; j < edges.length; j ++)
         {
-            this.render(e.children[i]);
-            r.path('M ' + e.x + ' ' + e.y + ' L ' + e.children[i].x + ' ' + e.children[i].y).attr({stroke: '#000', 'stroke-width': 2}).translate(250, 250);
+            if (edges[j].a == i || edges[j].b == i)
+                nodes[i].size ++;
         }
-        var c = r.circle(e.x, e.y, e.children.length * 2 + 8).attr({stroke: '#000', fill: '#f00'}).translate(250, 250);
-        c.mouseover(function(event) { this.attr({fill: '#fff'}); });
-        c.mouseout(function(event) { this.attr({fill: '#f00'}); });
+    }
+    
+    this.render = function()
+    {
+        for (var i = 0; i < edges.length; i ++)
+        {
+            var a = nodes[edges[i].a];
+            var b = nodes[edges[i].b];
+            r.path('M ' + a.x + ' ' + a.y + ' L ' + b.x + ' ' + b.y).attr({stroke: '#000', 'stroke-width': 2}).translate(250, 250);
+        }
+        for (var i = 0; i < nodes.length; i ++)
+        {
+            var e = nodes[i];
+            var c = r.circle(e.x, e.y, e.size * 2 + 8).attr({stroke: '#000', fill: '#f0' + i}).translate(250, 250);
+            c.mouseover(function(event) { this.attr({fill: '#fff'}); });
+            c.mouseout(function(event) { this.attr({fill: '#f00'}); });        
+        }
     }
     
     this.timer = setInterval(function()
     {
-        var nodes = flatten_graph(root);
         var debug = '';
 
         for (var i = 0; i < nodes.length; i ++)
         {
-            var force = { x: 0, y: 0 };
-            for (var j = 0; j < nodes.length; j ++)
+            for (var j = i + 1; j < nodes.length; j ++)
             {
-                var direction = 1;
-                if (i == j)
-                    continue;
-                if (nodes[i].children.indexOf(nodes[j]) != -1)
-                    continue;
-                    
                 var dx = nodes[i].x - nodes[j].x;
                 var dy = nodes[i].y - nodes[j].y;
                 var dsq = (dx * dx + dy * dy);
                 if (dsq == 0) dsq = 0.1; // avoid divide-by-zero
-                var push = 0;
+                var push = 50 / dsq;
 
-                if (nodes[j].children.indexOf(nodes[i]) != -1)
-                    push = -dsq / 50000;
-                else
-                    push = 50 / dsq;
-
-                force.x += dx * push;
-                force.y += dy * push;
+                nodes[i].force.x += dx * push;
+                nodes[i].force.y += dy * push;
+                
+                nodes[j].force.x -= dx * push;
+                nodes[j].force.y -= dx * push;
             }
-            nodes[i].x += force.x;
-            nodes[i].y += force.y;
+            
+            for (var j = 0; j < edges.length; j ++)
+            {
+                if (edges[j].a != i && edges[j].b != i)
+                    continue;
+                var a = edges[j].a;
+                var b = edges[j].b;
+                
+                var dx = nodes[a].x - nodes[b].x;
+                var dy = nodes[a].y - nodes[b].y;
+                var dsq = (dx * dx + dy * dy);
+                if (dsq == 0) dsq = 0.1; // avoid divide-by-zero
+                var push = -dsq / 50000;
+
+                nodes[a].force.x += dx * push;
+                nodes[a].force.y += dy * push;
+                
+                nodes[b].force.x -= dx * push;
+                nodes[b].force.y -= dx * push;
+            }
         }
 
+        for (var i = 0; i < nodes.length; i ++)
+        {
+            nodes[i].x += nodes[i].force.x;
+            nodes[i].y += nodes[i].force.y;
+            nodes[i].force.x = nodes[i].force.y = 0;
+        }
+        
         $('#debug').html(debug);
         
         r.clear();
-        this.render(root);
+        this.render();
     },
     100);
     
-    this.render(root);
-}
-
-function flatten_graph(node)
-{
-    var result = [];
-    result.push(node);
-    for (var i = 0; i < node.children.length; i ++)
-        result = result.concat(flatten_graph(node.children[i]));
-    return result;
+    this.render();
 }
